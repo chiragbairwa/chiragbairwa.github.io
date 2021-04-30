f678956f808b74a4024093c471118c72efd1fe2c
Chirag Bairwa <chiragbairwa2k18@gmail.com>
2021-04-30 21:11:05 +0530

    Skeleton 80%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Nav Bar */}
+      <nav className={classes.NavBar}>
+        {/* LOGO */}
+        <div>                    
+          <img src={logo} alt={"Dp"}></img>
+        </div>        
+        
+          {/* Links */}
+          <div className={classes.LinkBar }>
+            <a href={"http://localhost:3000/"}>Skills   </a>
+            <a href={"http://localhost:3000/"}>Projects   </a>
+            <a href={"http://localhost:3000/"}>About</a>
+          </div>
+                  
+      </nav>
+      
+      {/* Social Links */}
+        <div className={classes.SocialLinks}></div>
+            
+      {/* Welcome */}
+      <div className={classes.WelcomeCard}>
+        <div className={classes.WelcomeMsg}>
+          <h1>I'm<br></br>CHIRAG,Welcome</h1>
+        </div>
+        
+        {/* Show My Skill Button */}
+          <button>My Skills</button>
+        
+      </div>
+
+      {/* Skills Column */}
+      <div>
+        <h1> Skills </h1>
+        <div className={classes.PieChart}>
+
+        </div>
+      </div>
+
+      {/* Projects Column */}
+      <div>
+        <h1> Projects </h1>
+        <div >
+          <div className={classes.ProjectCards}> </div>
+          <div className={classes.ProjectCards}> </div>
+          <div className={classes.ProjectCards}> </div>
+          <div className={classes.ProjectCards}> </div>
+          <div className={classes.ProjectCards}> </div>
+        </div>
+
+      </div>
+
+      {/* Social Links */}
+      <div className={classes.SocialLinks}></div>
+
+      
     </div>
   );
 }
