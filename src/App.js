9f880be8364b97ebed5ec89890fbff6ad0726342
Chirag Bairwa <chiragbairwa2k18@gmail.com>
2021-05-02 19:22:13 +0530

    +About and PieChart init

--- src/App.js
+++ src/App.js
@@ -9,45 +9,63 @@ function App() {
           <img src={logo} alt={"Dp"} />
 
           {/* Nav Bar */}
-          <nav className={classes.NavBar}>   
+          <nav className={classes.NavBar}> 
             <a href={"http://localhost:3000/"}>Skills</a>
             <a href={"http://localhost:3000/"}>Projects</a>
             <a href={"http://localhost:3000/"}>About</a>
           </nav>
         </div>
               
-        {/* Welcome */}
+        {/* Welcome View*/}
         <div className={classes.WelcomeView}>
           <div className={classes.WelcomeMsg}>
-            <h1>I'm<br></br>CHIRAG,Welcome</h1>
+            <h1>Hey I'm<br></br>CHIRAG, Welcome</h1>
           </div>
           
-          {/* Show My Skill Button */}
-            <button className={classes.SkillBtn}>My Skills</button>
-          
+          {/* Show Buttons */}
+          <div className={classes.WelcomeBtn}>
+            <button className={classes.SkillBtn}>My Skills</button>  
+            <button className={classes.SkillBtn}>My Cv</button>    
+          </div>    
         </div>
 
-        {/* Skills Column */}
+        {/* Skills View */}
         <div className={classes.SkillView}>
           <h1> Skills </h1>
-          <div className={classes.PieChart}>
+          
+          <div className={classes.SkillCard}>
+            <div className={classes.PieChart}>
+
+            </div>
           </div>
         </div>
 
-        {/* Projects Column */}
+        {/* Projects View */}
         <div className={classes.ProjectView}>
           <h1> Projects </h1>
-
           {/* Cards */}
           <div className={classes.ProjectCards}>
-            <div> <p>Lorem</p> </div>
-            <div> <p>Lorem</p> </div>
+            <div>
+              <p>C/C++</p>
+              <p>Python</p>
+            </div>
+            <div>
+              <p>Java</p>
+              <p>React</p>
+            </div>            
           </div>
         </div>
 
         {/* Social Links */}
-        <div className={classes.SocialLinks}>
-        </div>        
+        {/* <div className={classes.SocialLinks}>
+          <p>Lor</p>
+          <p>Lor</p>
+          </div>  */}      
+      </div>
+
+      {/* About */}
+      <div className={classes.AboutView}>
+        <h2>About</h2>
       </div>
     </div>
   );
