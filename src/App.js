06c5276f5b99f93fbfeb6e4757f49d20de500b5b
chirag <chirag.dev18@gmail.com>
2022-01-18 21:36:43 +0530

    New Landing Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import classes from './App.module.css';
+import classes from './App.module.css'
 
 // Importing Components
-import Welcome from './comp/welcome';
-import Skill from './comp/skill';
-import Project from './comp/project';
-import Header from './comp/Header';
+import Welcome from './comp/welcome'
+import Skill from './comp/skill'
+import Project from './comp/project'
+import Header from './comp/Header'
 
 export default function App() {
   return (
     <div className="App">
       <div className={classes.Container}>
-        {/* Header */}
-        <Header />
-              
-        {/* Welcome View*/}
-        <Welcome />
+        <div className={classes.LandingPage}>
+          {/* Header */}
+          <Header />
+
+          {/* Welcome View*/}
+          <Welcome />
+        </div>
 
         {/* Skills View */}
         <Skill />
-        
+
         {/* Projects View */}
-        <Project id="ProjectView"/>
+        <Project id="ProjectView" />
       </div>
 
       {/* About */}
@@ -31,33 +33,47 @@ export default function App() {
             <h4>Feel free to contact me!!</h4>
             <div>
               <p>
-                <a href="mailto:chirag.dev18@gmail.com">chirag.dev18@gmail.com</a><br></br>
+                <a href="mailto:chirag.dev18@gmail.com">
+                  chirag.dev18@gmail.com
+                </a>
+                <br></br>
                 Chandigarh, India
               </p>
             </div>
           </div>
-        
+
           {/* Social Links */}
           <div className={classes.Social}>
             <h4>Links</h4>
             <div className={classes.SocialBtn}>
-              <a href={"https://github.com/chiragbairwa"} target="_blank" rel="noopener noreferrer">              
+              <a
+                href={'https://github.com/chiragbairwa'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <div className={classes.github}></div>
               </a>
-              <a href={"https://linkedin.com/in/chiragbairwa"} target="_blank" rel="noopener noreferrer">              
+              <a
+                href={'https://linkedin.com/in/chirag-bairwa'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <div className={classes.linkedin}></div>
               </a>
-              <a href={"https://instagram.com/x.chirag_x"} target="_blank" rel="noopener noreferrer">              
+              <a
+                href={'https://instagram.com/hi.chirag'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <div className={classes.instagram}></div>
               </a>
             </div>
-          </div> 
+          </div>
         </div>
         {/* Copyright */}
         <hr></hr>
         <p>© Made with ❤️ by Chirag</p>
       </div>
-      
     </div>
-  );
+  )
 }
--- src/comp/Header.js
+++ src/comp/Header.js
@@ -1,64 +1,79 @@
 // Modules
-import { useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Link from 'react-scroll/modules/components/Link';
-import classes from '../App.module.css';
+import { useEffect } from 'react'
+import Button from 'react-bootstrap/Button'
+import Dropdown from 'react-bootstrap/Dropdown'
+import Link from 'react-scroll/modules/components/Link'
+import classes from '../App.module.css'
 
-import logo from '../images/dp.png';
+import logo from '../images/dp.png'
 
-export default function Header(){ 
-
-  useEffect(()=>{
-    window.onscroll = ()=>{
-      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").style.backgroundColor = '#191c2b';
-        document.getElementById("header").style.paddingTop = '0';
-        document.getElementById("header").style.opacity = '0.99';
-      } 
-      else {
-        document.getElementById("header").style.backgroundColor= '#23263A';
-        document.getElementById("header").style.paddingTop = '20px';
-        document.getElementById("header").style.opacity = '1';
+export default function Header() {
+  useEffect(() => {
+    window.onscroll = () => {
+      if (
+        document.body.scrollTop > 50 ||
+        document.documentElement.scrollTop > 50
+      ) {
+        document.getElementById('header').style.backgroundColor = 'black'
+        document.getElementById('header').style.paddingTop = '0'
+        document.getElementById('header').style.opacity = '0.99'
+      } else {
+        document.getElementById('header').style.backgroundColor = 'transparent'
+        document.getElementById('header').style.paddingTop = '20px'
+        document.getElementById('header').style.opacity = '1'
       }
     }
-  },[]);
-  
-	return(
+  }, [])
+
+  return (
     <div className={classes.Header} id="header">
-      <img src={logo} alt={"Dp"} className={classes.Dp} />
+      <img src={logo} alt={'Dp'} className={classes.Dp} />
       {/* Nav Bar */}
       <nav className={classes.NavBar}>
-        <Link id="SkillBtn" to="SkillView" smooth={true} offset={-115} duration={500} >
-          <Button variant="outline-light" >Skills</Button>
+        <Link
+          id="SkillBtn"
+          to="SkillView"
+          smooth={true}
+          offset={-115}
+          duration={500}
+        >
+          <Button variant="outline-light">Skills</Button>
         </Link>
-        <Link id="ProjectBtn" to="ProjectView" smooth={true} offset={-65} duration={500} >
-          <Button variant="outline-light" >Projects</Button>
+        <Link
+          id="ProjectBtn"
+          to="ProjectView"
+          smooth={true}
+          offset={-65}
+          duration={500}
+        >
+          <Button variant="outline-light">Projects</Button>
         </Link>
-				<Link id="AboutBtn" to="AboutView" smooth={true} duration={500} >
-          <Button variant="outline-light" >About</Button>
-        </Link>			
-			</nav>
+        <Link id="AboutBtn" to="AboutView" smooth={true} duration={500}>
+          <Button variant="outline-light">About</Button>
+        </Link>
+      </nav>
 
       {/* Mobile NavBar */}
       <nav className={classes.NavBarMob}>
-          <Dropdown>
-            <Dropdown.Toggle variant="light" id="dropdown-basic">
-            </Dropdown.Toggle>
+        <Dropdown>
+          <Dropdown.Toggle
+            variant="light"
+            id="dropdown-basic"
+          ></Dropdown.Toggle>
 
-            <Dropdown.Menu variant="light" className={classes.Dropdown}>
-              <Link to="SkillView" smooth={true} offset={-80} duration={500} >
-                Skills
-              </Link>
-              <Link to="ProjectView" smooth={true} offset={-70} duration={500} >
-                Projects
-              </Link>
-              <Link to="AboutView" smooth={true} duration={500} >
-                About
-              </Link>
-            </Dropdown.Menu>
-          </Dropdown>
+          <Dropdown.Menu variant="light" className={classes.Dropdown}>
+            <Link to="SkillView" smooth={true} offset={-80} duration={500}>
+              Skills
+            </Link>
+            <Link to="ProjectView" smooth={true} offset={-70} duration={500}>
+              Projects
+            </Link>
+            <Link to="AboutView" smooth={true} duration={500}>
+              About
+            </Link>
+          </Dropdown.Menu>
+        </Dropdown>
       </nav>
     </div>
-	);
+  )
 }
