eeb700de2de588139869b072746cb1173e3d972c
chirag <chirag.dev18@gmail.com>
2022-01-28 20:39:20 +0530

    logo size reduced header color changed Mob Navbar navigations corrected

--- src/comp/Header.js
+++ src/comp/Header.js
@@ -14,7 +14,7 @@ export default function Header() {
         document.body.scrollTop > 50 ||
         document.documentElement.scrollTop > 50
       ) {
-        document.getElementById('header').style.backgroundColor = 'black'
+        document.getElementById('header').style.backgroundColor = '#00101b'
         document.getElementById('header').style.paddingTop = '0'
         document.getElementById('header').style.opacity = '0.99'
       } else {
@@ -27,7 +27,7 @@ export default function Header() {
 
   return (
     <div className={classes.Header} id="header">
-      <img src={logo} alt={'Dp'} className={classes.Dp} />
+      <img src={logo} alt={'Dp'} className={classes.Dp} id="Dp" />
       {/* Nav Bar */}
       <nav className={classes.NavBar}>
         <Link
@@ -62,10 +62,10 @@ export default function Header() {
           ></Dropdown.Toggle>
 
           <Dropdown.Menu variant="light" className={classes.Dropdown}>
-            <Link to="SkillView" smooth={true} offset={-80} duration={500}>
+            <Link to="SkillView" smooth={true} offset={-60} duration={500}>
               Skills
             </Link>
-            <Link to="ProjectView" smooth={true} offset={-70} duration={500}>
+            <Link to="ProjectView" smooth={true} offset={-80} duration={500}>
               Projects
             </Link>
             <Link to="AboutView" smooth={true} duration={500}>
